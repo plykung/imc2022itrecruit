2915c90cc52776cfe846b814ec99f987872a0fa1
plykung <58039748+plykung@users.noreply.github.com>
2023-02-26 03:48:13 +0700

    use dotenv

--- utils/firebase/fbinit.js
+++ utils/firebase/fbinit.js
@@ -1,5 +1,18 @@
 import { initializeApp } from "firebase/app";
-import firebaseConfig from "../../firebaseConfig.json";
+// import firebaseConfig from "../../firebaseConfig.json";
+// dotenv
+import * as dotenv from 'dotenv'
+dotenv.config()
+
+var firebaseConfig = {
+    "apiKey": process.env.API_KEY,
+    "authDomain": process.env.AUTH_DOMAIN,
+    "projectId": process.env.PROJECT_ID,
+    "storageBucket": process.env.STORAGE_BUCKET,
+    "messagingSenderId": process.env.MESSAGING_SENDER_ID,
+    "appId": process.env.APP_ID,
+    "measurementId": process.env.MEASUREMENT_ID
+}
 
 const appfb = initializeApp(firebaseConfig);
 
